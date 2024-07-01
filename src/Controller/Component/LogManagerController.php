b4c69ca39c56483e0315b89fa8e8d5b4bd3bf673
Lukáš Bečvář <becvarlukas99@gmail.com>
2024-07-01 14:15:52 +0200

    [+] added empty log manager component

---
+++ src/Controller/Component/LogManagerController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller\Component;
+
+use App\Manager\AuthManager;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * Class LogManagerController
+ *
+ * Controller to manage or display of logs
+ * 
+ * @package App\Controller\Component
+ */
+class LogManagerController extends AbstractController
+{
+    private AuthManager $authManager;
+
+    public function __construct(AuthManager $authManager)
+    {
+        $this->authManager = $authManager;
+    }
+
+    /**
+     * Display the logs table view.
+     *
+     * @return Response The response object containing the rendered view.
+     */
+    #[Route('/manager/logs', methods:['GET'], name: 'app_manager_logs')]
+    public function logsTable(): Response
+    {
+        // return logs table view
+        return $this->render('component/log-manager/log-table.twig', [
+            'isAdmin' => $this->authManager->isLoggedInUserAdmin(),
+            'userData' => $this->authManager->getLoggedUserRepository()
+        ]);
+    }
+}
