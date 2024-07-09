94ee77f66d39b8a540c2dac650e602fe39093de6
Lukáš Bečvář <becvarlukas99@gmail.com>
2024-07-09 18:01:38 +0200

    [+] added todo entity datafixture

--- scripts/load-fixtures.sh
+++ scripts/load-fixtures.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# refresh database
+sh scripts/drop-database.sh
+sh scripts/migrate.sh
+
 # load testing data
 php bin/console doctrine:fixtures:load --no-interaction
 php bin/console doctrine:fixtures:load --no-interaction --env=test
