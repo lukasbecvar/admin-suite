f6535001b0f8bf4fff623cfce4b4f59291ed0dab
Lukáš Bečvář <becvarlukas99@gmail.com>
2024-07-10 15:22:15 +0200

    [+] added todo manager tests

---
+++ tests/Controller/Component/TodoManagerControllerTest.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Tests\Controller\Component;
+
+use App\Tests\CustomTestCase;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+
+/**
+ * Class TodoManagerControllerTest
+ *
+ * Test the todo manager component
+ *
+ * @package App\Tests\Controller\Component
+ */
+class TodoManagerControllerTest extends CustomTestCase
+{
+    private KernelBrowser $client;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->client = static::createClient();
+
+        $this->simulateLogin($this->client);
+    }
+
+    /**
+     * Test the todo manager load
+     *
+     * @return void
+     */
+    public function testTodoListLoad(): void
+    {
+        $this->client->request('GET', '/manager/todo');
+
+        // assert response
+        $this->assertSelectorTextContains('title', 'Admin suite');
+        $this->assertSelectorTextContains('body', 'Todo list');
+        $this->assertSelectorExists('input[name="create_todo_form[todo_text]"]');
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+    }
+
+    /**
+     * Test the todo item add with empty text
+     *
+     * @return void
+     */
+    public function testTodoItemAddEmptyText(): void
+    {
+        // make request
+        $this->client->request('POST', '/manager/todo', [
+            'create_todo_form' => [
+                'todo_text' => ''
+            ]
+        ]);
+
+        // assert response
+        $this->assertSelectorTextContains('body', 'Please enter a todo text');
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+    }
+
+    /**
+     * Test the todo item add with long text
+     *
+     * @return void
+     */
+    public function testTodoItemAddLongText(): void
+    {
+        // make request
+        $this->client->request('POST', '/manager/todo', [
+            'create_todo_form' => [
+                'todo_text' => 'asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfafff'
+            ]
+        ]);
+
+        // assert response
+        $this->assertSelectorTextContains('body', 'Your todo text cannot be longer than 255 characters');
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+    }
+
+    /**
+     * Test the todo item add
+     *
+     * @return void
+     */
+    public function testTodoItemAdd(): void
+    {
+        // make request
+        $this->client->request('POST', '/manager/todo', [
+            'create_todo_form' => [
+                'todo_text' => 'todo text'
+            ]
+        ]);
+
+        // assert response
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+    }
+}
