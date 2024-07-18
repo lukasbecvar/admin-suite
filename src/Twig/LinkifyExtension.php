9bc38964b88a7b41b17a4c1fadb8a2ec0f709c17
Lukáš Bečvář <becvarlukas99@gmail.com>
2024-07-18 14:40:20 +0200

    [+] added todo text linkify

---
+++ src/Twig/LinkifyExtension.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\TwigFilter;
+use Twig\Extension\AbstractExtension;
+
+/**
+ * Class LinkifyExtension
+ *
+ * LinkifyExtension is a custom Twig extension for converting links in text to clickable HTML links
+ *
+ * @package App\Twig
+ */
+class LinkifyExtension extends AbstractExtension
+{
+    /**
+     * Get the filters provided by this extension
+     *
+     * @return array<TwigFilter> An array of Twig filters
+     */
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('linkify', [$this, 'linkifyText'], ['is_safe' => ['html']]),
+        ];
+    }
+
+    /**
+     * Converts links in the given text to clickable HTML links
+     *
+     * @param string $text The input text
+     *
+     * @return string|null The text with clickable HTML links
+     */
+    public function linkifyText(string $text): ?string
+    {
+        return preg_replace(
+            '/(https?:\/\/[^\s]+)/',
+            '<a href="$1" class="linkify" target="_blank">$1</a>',
+            $text
+        );
+    }
+}
