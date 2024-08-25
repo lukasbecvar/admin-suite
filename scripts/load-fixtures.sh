0cc00a1cc3c4fb506b0e6ce5925d339d94731105
Lukáš Bečvář <lordbecvold@gmail.com>
2024-08-25 15:13:29 +0200

    [R] basic design improvements

--- scripts/load-fixtures.sh
+++ scripts/load-fixtures.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-#  drop database
+# check if environment is dev
+[ -f .env ] && export $(grep -v '^#' .env | xargs)
+[ "$APP_ENV" != "dev" ] && echo "This script is only for development environment" && exit 1
+
+# drop database
 sh scripts/drop-database.sh
 
 # migrate database to latest version
