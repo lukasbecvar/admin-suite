3255f28c9afb6b9834ee3fa777b0e65851a41f56
Lukáš Bečvář <lordbecvold@gmail.com>
2024-12-01 15:15:00 +0100

    design improvements

--- assets/js/loading-component.js
+++ assets/js/loading-component.js
@@ -3,3 +3,19 @@ document.addEventListener("DOMContentLoaded", function() {
     // hide loading component after page load
     document.getElementById("loader-wrapper").style.display = "none"
 })
+
+/* loading component for click on links */
+document.addEventListener("DOMContentLoaded", function () {
+    const loader = document.getElementById("loader-wrapper")
+    loader.style.display = "none"
+    document.body.addEventListener("click", function (event) {
+        const target = event.target.closest("a")
+        if (target && target.href) {
+            event.preventDefault()
+            loader.style.display = "flex"
+            setTimeout(() => {
+                window.location.href = target.href
+            }, 10)
+        }
+    })
+})
