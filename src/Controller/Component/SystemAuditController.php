5961174b61a3303208d95643e89119e1d8a813fa
Lukáš Bečvář <becvarlukas99@gmail.com>
2025-04-12 19:00:10 +0200

    added system audit page component

---
+++ src/Controller/Component/SystemAuditController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Controller\Component;
+
+use Exception;
+use App\Util\ServerUtil;
+use App\Manager\LogManager;
+use App\Manager\ErrorManager;
+use App\Annotation\Authorization;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * Class SystemAuditController
+ *
+ * Controller for system audit component
+ *
+ * @package App\Controller\Component
+ */
+class SystemAuditController extends AbstractController
+{
+    private LogManager $logManager;
+    private ServerUtil $serverUtil;
+    private ErrorManager $errorManager;
+
+    public function __construct(LogManager $logManager, ServerUtil $serverUtil, ErrorManager $errorManager)
+    {
+        $this->logManager = $logManager;
+        $this->serverUtil = $serverUtil;
+        $this->errorManager = $errorManager;
+    }
+
+    /**
+     * Render system audit component dashboard page
+     *
+     * @return Response The system audit page view
+     */
+    #[Authorization(authorization: 'ADMIN')]
+    #[Route('/system/audit', methods:['GET'], name: 'app_system_audit')]
+    public function systemAuditDashboard(): Response
+    {
+        // get system info data
+        $hostUptime = $this->serverUtil->getHostUptime();
+        $processList = $this->serverUtil->getProcessList();
+        $hostSystemInfo = $this->serverUtil->getSystemInfo();
+        $systemInstallInfo = $this->serverUtil->getSystemInstallInfo();
+
+        // get diagnostic data
+        $diagnosticData = $this->serverUtil->getDiagnosticData();
+
+        // get log files from host system
+        try {
+            $logFiles = $this->logManager->getSystemLogs();
+        } catch (Exception $e) {
+            $this->errorManager->handleError(
+                message: 'error to get system logs: ' . $e->getMessage(),
+                code: Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+
+        // return system audit dashboard page view
+        return $this->render('component/system-audit/audit-dashboard.twig', [
+            // system info data
+            'processList' => $processList,
+            'hostUptime' => $hostUptime,
+            'hostSystemInfo' => $hostSystemInfo,
+            'systemInstallInfo' => $systemInstallInfo,
+
+            // log files data
+            'logFiles' => $logFiles,
+
+            // diagnostic data
+            'diagnosticData' => $diagnosticData
+        ]);
+    }
+}
---
+++ tests/Controller/Component/SystemAuditControllerTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Tests\Controller\Component;
+
+use App\Tests\CustomTestCase;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+
+/**
+ * Class TerminalApiControllerTest
+ *
+ * Test cases for system audit component
+ *
+ * @package App\Tests\Controller\Component
+ */
+class SystemAuditControllerTest extends CustomTestCase
+{
+    private KernelBrowser $client;
+
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+
+        // simulate user authentication
+        $this->simulateLogin($this->client);
+    }
+
+    /**
+     * Test load system audit page
+     *
+     * @return void
+     */
+    public function testLoadSystemAuditPage(): void
+    {
+        $this->client->request('GET', '/system/audit');
+
+        // assert response
+        $this->assertSelectorTextContains('title', 'Admin suite');
+        $this->assertSelectorTextContains('body', 'System Audit');
+        $this->assertSelectorExists('a[title="Back to dashboard"]');
+        $this->assertSelectorExists('a[title="Go to diagnostics"]');
+        $this->assertSelectorExists('a[title="Go to metrics dashboard"]');
+        $this->assertSelectorTextContains('body', 'System Information');
+        $this->assertSelectorTextContains('body', 'Process list');
+        $this->assertSelectorTextContains('body', 'System logs');
+        $this->assertSelectorTextContains('body', 'System diagnostics');
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+    }
+}
